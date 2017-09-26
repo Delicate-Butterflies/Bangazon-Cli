98424c28ddc36e42e03a3fe5b42a10c08e1e3d09
Joshua Lloyd <joshualloyd@joshualloyd.com>
2017-09-26 14:54:16 -0500

    added dotenv setup

--- test/create-user.test.js
+++ test/create-user.test.js
@@ -1,11 +1,15 @@
+require('dotenv').config();
+
 const { assert: { equal, isFunction, isObject, isNotOk } } = require('chai');
 const { createTables, insertRows } = require('../db/buildDB');
 const { getUser, saveNewUser } = require('../app/models/user.js');
 
+let TIMEOUT = process.env.TIMEOUT;
+
 // Placed here to confirm test file runs properly
 describe('create a user', () => {
   before(function () {
-    this.timeout(10000);
+    this.timeout(TIMEOUT);
     return createTables().then(() => {
       // console.log('Create Then', msg);
       return insertRows();
