f19009d3e1b3ee58b3bb92d2b23ab2792f8cee28
Joshua Lloyd <joshualloyd@joshualloyd.com>
2017-09-26 07:29:08 -0500

    wip for db rebuild in test addition

--- test/customer.test.js
+++ test/customer.test.js
@@ -1,9 +1,22 @@
-const { assert: {equal} } = require('chai');
+const { assert: { equal } } = require('chai');
+const { createTables, insertRows } = require('../db/buildDB')
 
 // Placed here to confirm test file runs properly
-describe('just a test', () => {
+describe('just a test', function () {
+  this.timeout(9000);
+  beforeEach((done) => {
+    createTables()
+      .then((msg) => {
+        console.log('Create Then', msg);
+        return insertRows()
+      })
+      .then((idArr) => {
+        console.log('idArr', idArr);
+        done();
+      });
+  });
   it('should be equal', () => {
-    equal( 3, 1 + 2)
+    equal(3, 1 + 2)
   });
 });
 
