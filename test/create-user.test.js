c56b23a20ace551fd4dd3e070033711e8724187e
thejonroberts <jon.roberts.8@gmail.com>
2017-09-26 15:44:23 -0500

    removed old test file

--- test/create-user.test.js
+++ test/create-user.test.js
@@ -8,26 +8,24 @@ let TIMEOUT = process.env.TIMEOUT;
 
 // Placed here to confirm test file runs properly
 describe('create a user', () => {
-  before(function () {
-    this.timeout(TIMEOUT);
-    return createTables().then(() => {
-      // console.log('Create Then', msg);
-      return insertRows();
-    });
-  });
-  describe('get a user', () => {
-    it('is a function', () => {
-      isFunction(getUser);
-    });
-    it('should return an object', () => {
-      return getUser(1)
-        .then((data) => {
-          console.log('this one', data);
-          isObject(data);
-        });
-    });
-  });
+	after(function() {
+		this.timeout(TIMEOUT);
+		return createTables().then(() => {
+			// console.log('Create Then', msg);
+			return insertRows();
+		});
+	});
+	describe('get a user', () => {
+		it('is a function', () => {
+			isFunction(getUser);
+		});
+		it('should return an object', () => {
+			return getUser(1).then(data => {
+				console.log('this one', data);
+				isObject(data);
+			});
+		});
+	});
 });
-
 
 // Pro Tip: Remember, we are testing features, not functions. Require whichever modules you need to test a feature
