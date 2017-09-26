ec52465835794aacc449903a094745fe1f3584bd
Arwa <arwa.it15@gmail.com>
2017-09-26 15:12:41 -0500

    .env

--- app/controllers/add-payment-type-ctrl.js
+++ app/controllers/add-payment-type-ctrl.js
@@ -10,14 +10,16 @@ module.exports.promptAddPayment = () => {
           name: 'paymentType',
           pattern: /^[a-zA-Z\s\-]+$/,
           description: 'Enter payment type (visa, amex, etc)',
+          message: 'payment type must be letters',
           type: 'string',
           required: true
         },
         {
           name: 'accountNumber',
           description: 'Enter Account Number(16 digits)',
-          type: 'integer',
-          maxlength: 16,
+          pattern: /^(\d{16})$/,
+          type: 'number',
+          message: 'account number must be 16 digits long',
           required: true
         }
       ],
