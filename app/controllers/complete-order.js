0c479da0902fd924c82319997019db56b839cc2f
Megan B <brown.m.j@outlook.com>
2017-09-27 11:38:27 -0500

    prompt functions in complete-order controller

--- app/controllers/complete-order.js
+++ app/controllers/complete-order.js
@@ -1,19 +1,38 @@
 'use strict';
 
 const prompt = require('prompt');
+const { displayWelcome } = require('../app/ui.js')
 
 module.exports.promptCompleteOrder = () => {
   return new Promise((resolve, reject) => {
     // if there are no products
+    console.log('Please add some products to your order first. Press any key to return to main menu.');
+    // if there are products in the order
+    console.log('Your order total is $00.00. Ready to purchase?');
     prompt.get([
       {
-        message: 'Please add some products to your order first. Press any key to return to main menu.'
+        name: 'purchaseAnswer',
+        description: '(Y/N)'
       }
-    ]);
-    // if there are products in the order
-    prompt.get  , function(err, results) {
+    ]), function(err, results) {
       if (err) return reject(err);
-      resolve(results);
+      if (results.purchaseAnswer == ('Y' || 'y') {
+        promptChoosePaymentOption();
+      } else {
+        displayWelcome();
+      }
     });
   });
 };
+
+// if user enters Y
+promptChoosePaymentOption = () => {
+  dbGetUsersPaymentTypes().then(data => {
+    console.log('Choose a payment option');
+    for (let i = 0; i < data.length; i++) {
+      console.log(`${data[i].id}: ${data[i].type}`);
+    }
+  })
+}
+
+
