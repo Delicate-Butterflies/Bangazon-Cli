a3ad5f1005b58034e12446ff9c232e841525d5d9
Arwa <arwa.it15@gmail.com>
2017-09-27 11:53:33 -0500

    testing getter function in progress

--- test/add-payment-type.test.js
+++ test/add-payment-type.test.js
@@ -5,6 +5,11 @@ const { assert } = require('chai');
 const { dbPostPaymentType, dbGetOnePaymentType, dbDeleteOnePaymentType } = require('../app/models/Payment-Types.js');
 
 describe('post new user payment type', () => {
+  let paymentTypeObj = {
+    customer_user_id: 3,
+    type: 'visa',
+    account_number: 1234123412341234
+  };
   describe('getOnePaymentType', () => {
     it('should be a function', () => {
       assert.isFunction(dbGetOnePaymentType);
@@ -22,11 +27,6 @@ describe('post new user payment type', () => {
       });
 
       it('should add new payment type and should have expected id', () => {
-        let paymentTypeObj = {
-          customer_user_id: 3,
-          type: 'visa',
-          account_number: 1234123412341234
-        };
         let expected = 176;
         return dbPostPaymentType(paymentTypeObj).then(data => {
           let returnedData = data.new_payment_type_id;
@@ -35,7 +35,20 @@ describe('post new user payment type', () => {
         });
       });
 
-      it('should be able to get the data back using getter');
+      it('should confirm that the data is added by using getter', () => {
+        let expected = {
+          customer_user_id: 3,
+          type: 'visa',
+          account_number: 1234123412341234
+        };
+        return dbPostPaymentType(paymentTypeObj).then(data => {
+          dbGetOnePaymentType(data.new_payment_type_id).then(receivedData => {
+            console.log(receivedData);
+            assert.deepEqual(receivedData, expected);
+            dbDeleteOnePaymentType(176);
+          });
+        });
+      });
     });
   });
 });
