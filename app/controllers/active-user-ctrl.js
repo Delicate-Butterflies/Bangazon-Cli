a8bf4ed77287fd47ced4c3b1e2a8439bdd9a7bb7
Arwa <arwa.it15@gmail.com>
2017-10-01 15:02:17 -0500

    show current active user in main menu

--- app/controllers/active-user-ctrl.js
+++ app/controllers/active-user-ctrl.js
@@ -22,6 +22,9 @@ module.exports.promptPrintUsers = () => {
           if (err) return reject(err);
           if (activeUserIDs.toString().includes(results.activeUser)) {
             results.exists = true;
+            for (let i = 0; i < data.length; i++) {
+              if (data[i].id == results.activeUser) results.userName = `${data[i].first_name} ${data[i].last_name}`;
+            }
             resolve(results);
           } else {
             results.exists = false;
