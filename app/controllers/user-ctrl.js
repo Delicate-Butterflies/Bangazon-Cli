fd96a8d0418e9bd4394c1c833121bbfc0ba8f4a3
Arwa <arwa.it15@gmail.com>
2017-10-02 14:49:43 -0500

    work in progress

--- app/controllers/user-ctrl.js
+++ app/controllers/user-ctrl.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const prompt = require('prompt');
+const keypress = require('keypress');
 
 /* prompt info:
   description: 'Enter your password',     // Prompt displayed to the user. If not supplied name will be used.
@@ -18,6 +19,18 @@ let { dbPostUser } = require('../models/User.js');
 
 module.exports.promptNewUser = () => {
   return new Promise((resolve, reject) => {
+    console.log('Press esc to exit...');
+    process.stdin.setRawMode(true);
+    process.stdin.resume(); //https://stackoverflow.com/questions/19687407/press-any-key-to-continue-in-nodejs
+    process.stdin.on('data', key => {
+      let newKey = process.stdin.read(key);
+      console.log(newKey);
+      if (key.toString('utf-8') == null) {
+        prompt.stop();
+        resolve('You have returned to the main menu!');
+        // process.stdin.setRawMode(true);
+      }
+    });
     prompt.get(
       [
         {
