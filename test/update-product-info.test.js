9435ac0368f5a1dd8606cf273a95038b3b5be9d4
Joshua Lloyd <joshualloyd@joshualloyd.com>
2017-10-02 00:17:43 -0500

    added tests

--- test/update-product-info.test.js
+++ test/update-product-info.test.js
@@ -1,5 +1,5 @@
 const { assert } = require('chai');
-const { dbGetAllProductsBySellerID } = require('../app/models/product.js');
+const { dbGetAllProductsBySellerID, dbGetSingleProduct, dbPutProduct } = require('../app/models/product.js');
 
 describe('Update product information', () => {
   describe('dbGetAllProductsBySellerID', () => {
@@ -24,6 +24,48 @@ describe('Update product information', () => {
       return dbGetAllProductsBySellerID(6).then(data => {
         assert.deepEqual(data[0], expected);
       });
+    });
+  });
+  describe('dbGetSingleProduct', () => {
+    it('is a function', () => {
+      assert.isFunction(dbGetSingleProduct);
+    });
+    it('returns an expected product', () => {
+      let expected = {
+        id: 4,
+        product_type_id: 7,
+        price: 93.8,
+        title: 'Practical Soft Fish',
+        description: 'Voluptas et culpa nobis a incidunt.',
+        original_quantity: 81,
+        seller_user_id: 1
+      };
+      return dbGetSingleProduct(4).then(data => {
+        assert.deepEqual(data, expected);
+      });
+    });
+  });
+  describe('dbPutProduct', () => {
+    it('is a function', () => {
+      assert.isFunction(dbPutProduct);
+    });
+    it('updates a specific product', () => {
+      let expected = {
+        id: 10,
+        product_type_id: 4,
+        price: 57.1,
+        title: 'Large Soft Cheese',
+        description: 'it is tasty',
+        original_quantity: 99,
+        seller_user_id: 1
+      };
+      return dbPutProduct({ body: { title: 'Large Soft Cheese', description: 'it is tasty', original_quantity: 99 } }, 10)
+        .then(() => {
+          return dbGetSingleProduct(10);
+        })
+        .then((data) => {
+          assert.deepEqual(data, expected);
+        });
     });
   });
 });
