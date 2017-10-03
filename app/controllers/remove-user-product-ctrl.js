3dab44a539c755937dc559fb7e55699d41a7099f
thejonroberts <jon.roberts.8@gmail.com>
2017-10-03 14:36:19 -0500

    remove stray con logs, add press any key to continue to revenue report

--- app/controllers/remove-user-product-ctrl.js
+++ app/controllers/remove-user-product-ctrl.js
@@ -14,77 +14,74 @@ let { dbDeleteOpenOrderByProduct } = require('../models/Order-Product.js');
  * @return {promise} - resolves with a message that the product has been successfully updated, or rejects with error
  */
 module.exports.removeUserProduct = user_id => {
-  return new Promise((resolve, reject) => {
-    console.log('Press ctrl+"c" to exit at any time');
-    dbGetAllProductsByUser(user_id).then(data => {
-      if (data.length == 0) {
-        return reject(`${red('Customer has no products')}`);
-      }
-      let productArr = [];
-      console.log(`Customer #${user_id} - all Products:`);
-      data.forEach((product, index) => {
-        productArr.push(product);
-        console.log(`${index + 1}. ${product.title} (product ID ${product.id})`);
-      });
-      prompt.get(
-        [
-          {
-            name: 'number',
-            description: 'Please select Product'
-          }
-        ],
-        function(err, choice) {
-          if (err) {
-            return reject(err);
-          } else if (parseInt(choice.number) < 1 || choice.number > productArr.length) {
-            return reject(`${red(`\n  >>No product #${choice.number} listed<<`)}`);
-          } else {
-            let productId = productArr[choice.number - 1].id;
-            dbCheckForProductSales(productId).then(data => {
-              if (data.sold == 0 || data.sold == undefined) {
-                console.log('no products sold');
-                // TODO add sold < original quantity case
-                dbDeleteProduct(productId)
-                  .then(() => {
-                    dbDeleteOpenOrderByProduct(productId)
-                      .then(() => {
-                        resolve(`${blue(`\n Product id ${productId} removed from database`)}`);
-                      })
-                      .catch(err => {
-                        return reject('\nBack to Main Menu', err);
-                      });
-                  })
-                  .catch(err => {
-                    return reject('\nBack to Main Menu', err);
-                  });
-              } else if (data.sold > 0) {
-                let productUpdate = { original_quantity: data.sold };
-                console.log('productUpdate', productUpdate);
-                dbPutProduct(productUpdate, productId)
-                  .then(updateData => {
-                    console.log('updateData', updateData);
-                    // remove from all open orders (remove open order ordersProducts rows)
-                    dbDeleteOpenOrderByProduct(productId)
-                      .then(OPdata => {
-                        console.log('OPdata', OPdata);
-                        resolve(
-                          `${blue(`\n >>Removed available quantity of #${productId} and removed from open orders<<`)}`
-                        );
-                      })
-                      .catch(err => {
-                        return reject('\nBack to Main Menu', err);
-                      });
-                  })
-                  .catch(err => {
-                    return reject('\nBack to Main Menu', err);
-                  });
-              } else {
-                return reject(`${red('\n >>Removing product unsuccessfull; database error, please contact admin<<')}`);
-              }
-            });
-          }
-        }
-      );
-    });
-  });
+	return new Promise((resolve, reject) => {
+		console.log('Press ctrl+"c" to exit at any time');
+		dbGetAllProductsByUser(user_id).then(data => {
+			if (data.length == 0) {
+				return reject(`${red('Customer has no products')}`);
+			}
+			let productArr = [];
+			console.log(`Customer #${user_id} - all Products:`);
+			data.forEach((product, index) => {
+				productArr.push(product);
+				console.log(`${index + 1}. ${product.title} (product ID ${product.id})`);
+			});
+			prompt.get(
+				[
+					{
+						name: 'number',
+						description: 'Please select Product'
+					}
+				],
+				function(err, choice) {
+					if (err) {
+						return reject(err);
+					} else if (parseInt(choice.number) < 1 || choice.number > productArr.length) {
+						return reject(`${red(`\n  >>No product #${choice.number} listed<<`)}`);
+					} else {
+						let productId = productArr[choice.number - 1].id;
+						dbCheckForProductSales(productId).then(data => {
+							if (data.sold == 0 || data.sold == undefined) {
+								console.log('no products sold');
+								// TODO add sold < original quantity case
+								dbDeleteProduct(productId)
+									.then(() => {
+										dbDeleteOpenOrderByProduct(productId)
+											.then(() => {
+												resolve(`${blue(`\n Product id ${productId} removed from database`)}`);
+											})
+											.catch(err => {
+												return reject('\nBack to Main Menu', err);
+											});
+									})
+									.catch(err => {
+										return reject('\nBack to Main Menu', err);
+									});
+							} else if (data.sold > 0) {
+								let productUpdate = { original_quantity: data.sold };
+								dbPutProduct(productUpdate, productId)
+									.then(() => {
+										// remove from all open orders (remove open order ordersProducts rows)
+										dbDeleteOpenOrderByProduct(productId)
+											.then(() => {
+												resolve(
+													`${blue(`\n >>Removed available quantity of #${productId} and removed from open orders<<`)}`
+												);
+											})
+											.catch(err => {
+												return reject('\nBack to Main Menu', err);
+											});
+									})
+									.catch(err => {
+										return reject('\nBack to Main Menu', err);
+									});
+							} else {
+								return reject(`${red('\n >>Removing product unsuccessfull; database error, please contact admin<<')}`);
+							}
+						});
+					}
+				}
+			);
+		});
+	});
 };
--- app/controllers/user-revenue-ctrl.js
+++ app/controllers/user-revenue-ctrl.js
@@ -8,38 +8,44 @@
 let { dbSellerRevenue } = require('../models/User.js');
 
 module.exports.sellerRevenueReport = user_id => {
-  return new Promise((resolve, reject) => {
-    console.log("Press ctrl+'c' to go back to main menu at any point");
-    dbSellerRevenue(user_id)
-      .then(soldProducts => {
-        if (!soldProducts[0]) {
-          return reject(`No Sales to Report for User ${user_id}`);
-        } else {
-          console.log(`Revenue Report for ${soldProducts[0].first_name} ${soldProducts[0].last_name}`);
-          // store order #s that have been reported
-          let printedOrderIds = [];
-          let totalSales = 0;
-          soldProducts.forEach(product => {
-            // if the order # has not been printed, print line on report
-            if (printedOrderIds.indexOf(product.order_id) === -1) {
-              console.log(`Order #${product.order_id}-----------`);
-              printedOrderIds.push(product.order_id);
-            }
-            // log info for each product on order
-            console.log(
-              `${product.title}, ${product.quantity}, ${product.price.toFixed(2)}, total ${(product.price *
-                product.quantity).toFixed(2)}`
-            );
-            totalSales += product.price * product.quantity;
-          });
-          // fix rounding errors
-          totalSales = totalSales.toFixed(2);
-          console.log(`Total Sales (all Orders): ${totalSales}`);
-          resolve(`Revenue Report completed`);
-        }
-      })
-      .catch(err => {
-        return reject('\nBack to Main Menu', err);
-      });
-  });
+	return new Promise((resolve, reject) => {
+		console.log("Press ctrl+'c' to go back to main menu at any point");
+		dbSellerRevenue(user_id)
+			.then(soldProducts => {
+				if (!soldProducts[0]) {
+					return reject(`No Sales to Report for User ${user_id}`);
+				} else {
+					console.log(`Revenue Report for ${soldProducts[0].first_name} ${soldProducts[0].last_name}`);
+					// store order #s that have been reported
+					let printedOrderIds = [];
+					let totalSales = 0;
+					soldProducts.forEach(product => {
+						// if the order # has not been printed, print line on report
+						if (printedOrderIds.indexOf(product.order_id) === -1) {
+							console.log(`Order #${product.order_id}-----------`);
+							printedOrderIds.push(product.order_id);
+						}
+						// log info for each product on order
+						console.log(
+							`${product.title}, ${product.quantity}, ${product.price.toFixed(2)}, total ${(product.price *
+								product.quantity).toFixed(2)}`
+						);
+						totalSales += product.price * product.quantity;
+					});
+					// fix rounding errors
+					totalSales = totalSales.toFixed(2);
+					console.log(`Total Sales (all Orders): ${totalSales}`);
+					console.log('Press any key to continue.'); //https://stackoverflow.com/questions/19687407/press-any-key-to-continue-in-nodejs
+					process.stdin.setRawMode(true);
+					process.stdin.resume();
+					process.stdin.on('data', () => {
+						resolve('data');
+					});
+					resolve(`Revenue Report completed`);
+				}
+			})
+			.catch(err => {
+				return reject('\nBack to Main Menu', err);
+			});
+	});
 };
